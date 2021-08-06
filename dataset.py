1be0e4ee08c25c8fdb67737e1400356dda26898a
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2021-08-06 17:28:04 +0200

    Clip the front tails of the lips

--- dataset.py
+++ dataset.py
@@ -101,11 +101,31 @@ class TailClipper:
 
     @classmethod
     def clip_lower_lip_tails(cls, lower_lip, lower_incisor, reg_out=True, **kwargs):
-        # Remove the back tail of the lower lip using the lower incisor as the reference
+        # Remove the front tail of the lower lip using the lower incisor as the reference
         ref_idx = lower_incisor[:, 1].argmax()
         reference = lower_incisor[ref_idx]
 
         llip_cp = lower_lip.copy()
+
+        llip_1st_half = llip_cp[:25]
+        llip_2nd_half = llip_cp[25:]
+
+        keep_indices = np.where(llip_2nd_half[:, 1] < reference[1] + (5 / pixel_spacing))
+
+        tailless_llip = np.concatenate([
+            llip_1st_half,
+            llip_2nd_half[keep_indices]
+        ])
+
+        if reg_out:
+            reg_x, reg_y = regularize_Bsplines(tailless_llip, 3)
+            tailless_llip = np.array([reg_x, reg_y]).T
+
+        # Remove the back tail of the lower lip using the lower incisor as the reference
+        ref_idx = lower_incisor[:, 1].argmax()
+        reference = lower_incisor[ref_idx]
+
+        llip_cp = tailless_llip.copy()
 
         llip_1st_half = llip_cp[:25]
         llip_2nd_half = llip_cp[25:]
@@ -125,11 +145,27 @@ class TailClipper:
 
     @classmethod
     def clip_upper_lip_tails(cls, upper_lip, upper_incisor, reg_out=True, **kwargs):
-        # Remove the back tail of the upper lip using the upper incisor as the reference
+        # Remove the front tail of the upper lip using the upper incisor as the reference
         ref_idx = -1
         reference = upper_incisor[ref_idx]
 
         ulip_cp = upper_lip.copy()
+
+        ulip_1st_half = ulip_cp[:25]
+        ulip_2nd_half = ulip_cp[25:]
+
+        keep_indices = np.where(ulip_2nd_half[:, 1] > reference[1] - (10 / cls.PIXEL_SPACING))
+
+        tailless_ulip = np.concatenate([
+            ulip_1st_half,
+            ulip_2nd_half[keep_indices]
+        ])
+
+        # Remove the back tail of the upper lip using the upper incisor as the reference
+        ref_idx = -1
+        reference = upper_incisor[ref_idx]
+
+        ulip_cp = tailless_ulip.copy()
 
         ulip_1st_half = ulip_cp[:25]
         ulip_2nd_half = ulip_cp[25:]
