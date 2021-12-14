004092b45d0ec7b8d4e986410eb4c87c603c5e12
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2021-12-14 18:20:08 +0100

    Move pearsons_correlation to metrics

--- metrics.py
+++ metrics.py
@@ -2,6 +2,8 @@ import torch
 
 from vt_tracker.metrics import p2cp_mean
 
+from loss import EuclideanDistanceLoss
+
 
 def p2cp_distance(outputs, targets):
     # outputs: torch.Size([bs, seq_len, N_art, 2, N_samples])
@@ -24,3 +26,39 @@ def p2cp_distance(outputs, targets):
         results = torch.cat([results, batch_results.unsqueeze(dim=0)])
 
     return results
+
+
+def pearsons_correlation(outputs, targets):
+    eps = 1e-5
+    bs, seq_len, n_articulators, _, n_samples = targets.shape
+
+    x_outputs = outputs[:, :, :, 0, :]
+    y_outputs = outputs[:, :, :, 1, :]
+
+    x_targets = targets[:, :, :, 0, :]
+    y_targets = targets[:, :, :, 1, :]
+
+    x_outputs_mean = x_outputs.mean(dim=1).unsqueeze(1).repeat_interleave(seq_len, dim=1)
+    vx_outputs = x_outputs - x_outputs_mean
+
+    x_targets_mean = x_outputs.mean(dim=1).unsqueeze(1).repeat_interleave(seq_len, dim=1)
+    vx_targets = x_targets - x_targets_mean
+
+    x_corr = torch.sum(vx_outputs * vx_targets, dim=1) / (torch.sqrt(torch.sum(vx_outputs ** 2, dim=1)) * torch.sqrt(torch.sum(vx_targets ** 2, dim=1)) + eps)
+
+    y_outputs_mean = y_outputs.mean(dim=1).unsqueeze(1).repeat_interleave(seq_len, dim=1)
+    vy_outputs = y_outputs - y_outputs_mean
+
+    y_targets_mean = y_targets.mean(dim=1).unsqueeze(1).repeat_interleave(seq_len, dim=1)
+    vy_targets = y_targets - y_targets_mean
+
+    y_corr = torch.sum(vy_outputs * vy_targets, dim=1) / (torch.sqrt(torch.sum(vy_outputs ** 2, dim=1)) * torch.sqrt(torch.sum(vy_targets ** 2, dim=1)) + eps)
+
+    return x_corr, y_corr
+
+
+def euclidean_distance(outputs, targets):
+    # outputs: torch.Size([bs, seq_len, N_art, 2, N_samples])
+    # targets: torch.Size([bs, seq_len, N_art, 2, N_samples])
+    euclidean_distance_fn = EuclideanDistanceLoss(reduction="none")
+    return euclidean_distance_fn(outputs, targets).mean(dim=-1)
