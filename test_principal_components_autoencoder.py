0ceb59c81265d0c15c945b4c48287172da5b16f2
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2023-02-04 00:19:51 +0100

    Reorganize the main function arguments on test_principal_components_autoencoder script

--- test_principal_components_autoencoder.py
+++ test_principal_components_autoencoder.py
@@ -13,36 +13,45 @@ from phoneme_to_articulation.principal_components.models import Autoencoder
 from phoneme_to_articulation.principal_components.losses import RegularizedLatentsMSELoss
 
 
-def main(cfg):
+def main(
+    datadir,
+    batch_size,
+    seq_dict,
+    articulator,
+    model_params,
+    encoder_state_dict_fpath,
+    decoder_state_dict_fpath,
+    save_to,
+    clip_tails=True,
+    num_workers=0,
+):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    test_sequences = sequences_from_dict(cfg["datadir"], cfg["test_seq_dict"])
+    test_sequences = sequences_from_dict(datadir, seq_dict)
     test_dataset = PrincipalComponentsAutoencoderDataset(
-        datadir=cfg["datadir"],
+        datadir=datadir,
         sequences=test_sequences,
-        articulator=cfg["articulator"],
+        articulator=articulator,
         sync_shift=0,
         framerate=55,
-        clip_tails=cfg["clip_tails"]
+        clip_tails=clip_tails
     )
     test_dataloader = DataLoader(
         test_dataset,
-        batch_size=cfg["batch_size"],
+        batch_size=batch_size,
         shuffle=False,
-        worker_init_fn=set_seeds
+        worker_init_fn=set_seeds,
+        num_workers=num_workers
     )
 
-    best_autoencoder = Autoencoder(
-        in_features=100,
-        n_components=cfg["n_components"]
-    )
-    best_encoder_state_dict = torch.load(cfg["encoder_state_dict_fpath"], map_location=device)
+    best_autoencoder = Autoencoder(**model_params)
+    best_encoder_state_dict = torch.load(encoder_state_dict_fpath, map_location=device)
     best_autoencoder.encoder.load_state_dict(best_encoder_state_dict)
-    best_decoder_state_dict = torch.load(cfg["decoder_state_dict_fpath"], map_location=device)
+    best_decoder_state_dict = torch.load(decoder_state_dict_fpath, map_location=device)
     best_autoencoder.decoder.load_state_dict(best_decoder_state_dict)
     best_autoencoder.to(device)
 
-    test_outputs_dir = os.path.join(cfg["save_to"], "test_outputs")
+    test_outputs_dir = os.path.join(save_to, "test_outputs")
     if not os.path.exists(test_outputs_dir):
         os.makedirs(test_outputs_dir)
 
@@ -53,11 +62,12 @@ def main(cfg):
         model=best_autoencoder,
         dataloader=test_dataloader,
         criterion=loss_fn,
-        outputs_dir=test_outputs_dir,
+        # outputs_dir=test_outputs_dir,
+        plots_dir=save_to,
         device=device
     )
 
-    with open(os.path.join(cfg["save_to"], "test_results.json"), "w") as f:
+    with open(os.path.join(save_to, "test_results.json"), "w") as f:
         ujson.dump(info_test, f)
 
 
@@ -69,4 +79,4 @@ if __name__ == "__main__":
     with open(args.cfg_filepath) as f:
         cfg = yaml.safe_load(f.read())
 
-    main(cfg)
+    main(**cfg)
