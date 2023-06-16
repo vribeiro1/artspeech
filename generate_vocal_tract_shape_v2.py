4f097da3a50542414010b7223dd56fea12076003
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2023-06-16 13:15:46 +0200

    Fix bugs on the generate_vocal_tract_shape.py and create version 2

--- generate_vocal_tract_shape_v2.py
+++ generate_vocal_tract_shape_v2.py
@@ -1,5 +1,3 @@
-import pdb
-
 import argparse
 import cv2
 import funcy
@@ -9,6 +7,7 @@ import pandas as pd
 import os
 import torch
 import ujson
+import yaml
 
 from functools import partial
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -17,23 +16,14 @@ from tgt import read_textgrid
 from tqdm import tqdm
 from vt_tools import (
     COLORS,
-    ARYTENOID_CARTILAGE,
-    EPIGLOTTIS,
-    LOWER_INCISOR,
-    LOWER_LIP,
-    PHARYNX,
-    SOFT_PALATE_MIDLINE,
-    THYROID_CARTILAGE,
-    TONGUE,
     UPPER_INCISOR,
-    UPPER_LIP,
-    VOCAL_FOLDS
 )
 from vt_tools.bs_regularization import regularize_Bsplines
 
 from vt_shape_gen.vocal_tract_tube import generate_vocal_tract_tube
-from helpers import npy_to_xarticul
+from helpers import npy_to_xarticul, sequences_from_dict
 from phoneme_to_articulation.phoneme_wise_mean_contour import forward_mean_contour
+from phoneme_to_articulation.encoder_decoder.dataset import ArtSpeechDataset
 from phoneme_to_articulation.encoder_decoder.models import ArtSpeech
 from settings import DATASET_CONFIG, BLANK, UNKNOWN
 
@@ -107,8 +97,7 @@ def make_frame(outputs, phoneme, regularize_outputs=True):
     return frame
 
 
-def make_vocal_tract_shape_video(outputs, phonemes, save_to, regularize_outputs=True, framerate=55):
-    video_filepath = os.path.join(save_to, f"{i}.avi")
+def make_vocal_tract_shape_video(outputs, phonemes, video_filepath, regularize_outputs=True, framerate=55):
     video_writer = cv2.VideoWriter(
         video_filepath,
         cv2.VideoWriter_fourcc(*"DIVX"),
@@ -117,7 +106,7 @@ def make_vocal_tract_shape_video(outputs, phonemes, save_to, regularize_outputs=
     )
 
     np_outputs = outputs.detach().cpu().numpy()
-    for i_frame, (frame_outputs, phoneme) in enumerate(zip(np_outputs, phonemes)):
+    for _, (frame_outputs, phoneme) in enumerate(zip(np_outputs, phonemes)):
         frame = make_frame(frame_outputs, phoneme, regularize_outputs)
         video_writer.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
@@ -144,13 +133,13 @@ def save_vocal_tract_shape(outputs, phonemes, save_to, regularize_outputs=True):
         plt.close()
 
 
-def save_contours(outputs, save_to, articulators, regularize_outputs=True):
+def save_contours(outputs, reference_arrays, save_to, articulators, regularize_outputs=True):
     filepaths = []
     np_outputs = outputs.detach().cpu().numpy()
-    for i_frame, frame_outputs in enumerate(np_outputs):
+
+    for i_frame, (frame_outputs, frame_reference) in enumerate(zip(np_outputs, reference_arrays), start=1):
         articul_dicts = {}
-        for i_articul, articul_arr in enumerate(frame_outputs):
-            articulator = articulators[i_articul]
+        for articulator, articul_arr in zip(articulators, frame_outputs):
             if regularize_outputs:
                 reg_X, reg_Y = regularize_Bsplines(articul_arr.T, 3)
                 articul_arr = np.array([reg_X, reg_Y])
@@ -159,133 +148,139 @@ def save_contours(outputs, save_to, articulators, regularize_outputs=True):
             np.save(filepath, articul_arr)
             articul_dicts[articulator] = filepath
 
+        if UPPER_INCISOR not in articulators:
+            filepath = os.path.join(save_to, f"{'%04d' % i_frame}_{UPPER_INCISOR}.npy")
+            articul_arr = frame_reference.squeeze(dim=0).numpy()
+            np.save(filepath, articul_arr)
+            articul_dicts[UPPER_INCISOR] = filepath
+
         filepaths.append(articul_dicts)
 
     return filepaths
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--database-name", dest="database_name", required=True)
-    parser.add_argument("--textgrid", dest="textgrid_filepath", required=True)
-    parser.add_argument("--encoding", dest="textgrid_encoding", default="utf-8")
-    parser.add_argument("--method", dest="method", default="neural-network")
-    parser.add_argument("--state-dict", dest="state_dict_filepath", required=True)
-    parser.add_argument("--vocabulary", dest="vocab_filepath", required=True)
-    parser.add_argument("--save-to", dest="save_to", required=True)
-    args = parser.parse_args()
-
-    validate_textgrid(args.textgrid_filepath, args.textgrid_encoding)
-    dataset_config = DATASET_CONFIG[args.database_name]
+def main(
+    database_name,
+    datadir,
+    seq_dict,
+    method,
+    state_dict_filepath,
+    vocab_filepath,
+    articulators,
+    save_to,
+):
+    dataset_config = DATASET_CONFIG[database_name]
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     default_tokens = [BLANK, UNKNOWN]
     vocabulary = {token: i for i, token in enumerate(default_tokens)}
-    with open(args.vocab_filepath) as f:
+    with open(vocab_filepath) as f:
         tokens = ujson.load(f)
         for i, token in enumerate(tokens, start=len(vocabulary)):
             vocabulary[token] = i
 
-    textgrid_filename, _ = os.path.basename(args.textgrid_filepath).split(".")
-    textgrid = read_textgrid(args.textgrid_filepath, args.textgrid_encoding)
-    phonetic_sequences = get_phonetic_sequences(textgrid)
-
-    unknown_index = vocabulary[UNKNOWN]
-    sentences_numerized = [
-        torch.tensor([
-            vocabulary.get(token, unknown_index) for token in sentence_tokens
-        ], dtype=torch.long)
-        for sentence_tokens in phonetic_sequences
-    ]
-
-    if args.method == "neural-network":
+    if method == "neural-network":
         model = ArtSpeech(len(vocabulary), 10)
-        state_dict = torch.load(args.state_dict_filepath, map_location=device)
+        state_dict = torch.load(state_dict_filepath, map_location=device)
         model.load_state_dict(state_dict)
         model.to(device)
-    elif args.method == "mean-contour":
+    elif method == "mean-contour":
         articulators = sorted(COLORS.keys())
 
-        df = pd.read_csv(args.state_dict_filepath)
+        df = pd.read_csv(state_dict_filepath)
         for articulator in articulators:
             df[articulator] = df[articulator].apply(eval)
 
         model = partial(forward_mean_contour, df=df, articulators=articulators)
     else:
-        raise Exception(f"Unavailable method '{args.method}'")
+        raise Exception(f"Unavailable method '{method}'")
 
-    progress_bar = tqdm(
-        enumerate(zip(sentences_numerized, phonetic_sequences), start=1),
-        total=len(phonetic_sequences)
+    sequences = sequences_from_dict(datadir, seq_dict)
+    dataset = ArtSpeechDataset(
+        datadir,
+        database_name,
+        sequences,
+        vocabulary,
+        articulators,
+        clip_tails=True,
     )
-    for i, (inputs, phonemes) in progress_bar:
-        inputs = inputs.unsqueeze(dim=0).to(device)
-        _, seq_len = inputs.shape
-        lengths = torch.tensor([seq_len], dtype=torch.long).cpu()
+    progress_bar = tqdm(dataset, desc="Synthesizing vocal tract")
 
-        if args.method == "neural-network":
-            outputs = model(inputs, lengths)
-        elif args.method == "mean-contour":
-            outputs = model(phonemes)
+    for (
+        sentence_name,
+        sentence_numerized,
+        _,
+        sentence_tokens,
+        reference_arrays,
+        _,
+        _,
+        _
+    ) in progress_bar:
+        subject_sequence, _ = sentence_name.split("-")
+        subject, _ = subject_sequence.split("_")
 
-        save_sentence_dir = os.path.join(args.save_to, textgrid_filename, "%04d" % i)
+        save_sentence_dir = os.path.join(save_to, subject, sentence_name)
         if not os.path.exists(save_sentence_dir):
             os.makedirs(save_sentence_dir)
-
-        with open(os.path.join(save_sentence_dir, "target_sequence.txt"), "w") as f:
-            f.write(" ".join(phonemes))
-
-        save_contours_dir = os.path.join(save_sentence_dir, "contours")
+        save_contours_dir = os.path.join(save_sentence_dir, "inference_contours")
         if not os.path.exists(save_contours_dir):
             os.makedirs(save_contours_dir)
-
+        save_air_column_dir = os.path.join(save_sentence_dir, "air_column")
+        if not os.path.exists(save_air_column_dir):
+            os.makedirs(save_air_column_dir)
         save_plots_dir = os.path.join(save_sentence_dir, "vocal_tract_shapes")
         if not os.path.exists(save_plots_dir):
             os.makedirs(save_plots_dir)
-        save_vocal_tract_shape(outputs.squeeze(dim=0), phonemes, save_plots_dir)
-        make_vocal_tract_shape_video(outputs.squeeze(dim=0), phonemes, save_sentence_dir)
-
         xarticul_dir = os.path.join(save_sentence_dir, "xarticul")
         if not os.path.exists(xarticul_dir):
             os.makedirs(xarticul_dir)
 
-        articulators = sorted([
-            ARYTENOID_CARTILAGE,
-            EPIGLOTTIS,
-            LOWER_INCISOR,
-            LOWER_LIP,
-            PHARYNX,
-            SOFT_PALATE_MIDLINE,
-            THYROID_CARTILAGE,
-            TONGUE,
-            UPPER_INCISOR,
-            UPPER_LIP,
-            VOCAL_FOLDS
-        ])
+        with open(os.path.join(save_sentence_dir, "target_sequence.txt"), "w") as f:
+            f.write(" ".join(sentence_tokens))
+
+        sentence_numerized = sentence_numerized.to(device)
+        seq_len = len(sentence_tokens)
+        lengths = torch.tensor([seq_len], dtype=torch.long).cpu()
+
+        if method == "neural-network":
+            sentence_numerized = sentence_numerized.unsqueeze(dim=0)
+            outputs = model(sentence_numerized, lengths)
+        elif method == "mean-contour":
+            outputs = model(sentence_tokens)
+
+        video_filepath = os.path.join(save_sentence_dir, f"{sentence_name}.avi")
+        save_vocal_tract_shape(outputs.squeeze(dim=0), sentence_tokens, save_plots_dir)
+        make_vocal_tract_shape_video(outputs.squeeze(dim=0), sentence_tokens, video_filepath)
+
         articulators_dicts = save_contours(
             outputs.squeeze(dim=0),
+            reference_arrays,
             save_contours_dir,
             articulators
         )
-        for i_frame, articuls_dict in enumerate(articulators_dicts):
+        for i_frame, articuls_dict in enumerate(articulators_dicts, start=1):
             internal_wall, external_wall = generate_vocal_tract_tube(articuls_dict)
+            air_column = np.array([internal_wall, external_wall])
+
+            frame_id = "%04d" % i_frame
+            air_column_filepath = os.path.join(save_air_column_dir, f"{frame_id}.npy")
+            np.save(air_column_filepath, air_column)
 
             xarticul_int = npy_to_xarticul(internal_wall * dataset_config.RES)
             xarticul_ext = npy_to_xarticul(external_wall * dataset_config.RES)
-
-            plt.figure(figsize=(10, 10))
-
-            plt.plot(*zip(*internal_wall), lw=3, color="red")
-            plt.plot(*zip(*external_wall), lw=3, color="blue")
-
-            plt.ylim([1., 0.])
-            plt.xlim([0., 1.])
-            plt.axis("off")
-
-            plt.savefig(os.path.join(save_sentence_dir, f"{'%04d' % i_frame}_shape.jpg"))
-            plt.close()
 
             xarticul_array = xarticul_int + xarticul_ext
             xarticul_filepath = os.path.join(xarticul_dir, f"{'%04d' % i_frame}.txt")
             with open(xarticul_filepath, "w") as f:
                 f.write("\n".join(xarticul_array))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", dest="config_filepath", required=True)
+    args = parser.parse_args()
+
+    with open(args.config_filepath) as f:
+        cfg = yaml.safe_load(f)
+
+    main(**cfg)
